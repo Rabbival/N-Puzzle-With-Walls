980406518ff14e0f8313de4186fc59ca9f280d02
Rabbival <87331993+Rabbival@users.noreply.github.com>
2023-12-29 16:05:48 +0200

    some more fixes

--- src/logic/board_building/solved_board_builder.rs
+++ src/logic/board_building/solved_board_builder.rs
@@ -5,21 +5,23 @@ pub fn generate_solved_board(grid_side_length: u8) -> TileTypeBoard{
     let grid_side_length_u32 = grid_side_length as u32;
 
     //Testing wall locations
-    solved_board.set(&GridLocation{row: 1, col: 1}, TileType::Wall(0));
-    solved_board.set(&GridLocation{row: 1, col: 3}, TileType::Wall(1));
-    solved_board.set(&GridLocation{row: 3, col: 0}, TileType::Wall(2));
-    solved_board.set(&GridLocation{row: 3, col: 1}, TileType::Wall(3));
-    solved_board.set(&GridLocation{row: 3, col: 2}, TileType::Wall(4));
-    solved_board.set(&GridLocation{row: 3, col: 3}, TileType::Wall(5));
+    solved_board.set(
+        &GridLocation{row: 1, col: 1}, 
+        IndexedValue::<TileType>::new(TileType::Wall)
+    );
+    solved_board.set(
+        &GridLocation{row: 1, col: 3}, 
+        IndexedValue::<TileType>::new(TileType::Wall)
+    );
 
     for i in 0..grid_side_length_u32{
         for j in 0..grid_side_length_u32{
             let location = GridLocation::new(i as i32, j as i32);
-            solved_board.set_if_empty(&location, TileType::Numbered(0));
+            solved_board.set_if_empty(&location, IndexedValue::<TileType>::new(TileType::Numbered));
         }
     }
     let empty_tile_location=GridLocation::new((grid_side_length_u32-1) as i32, (grid_side_length_u32-1) as i32);
-    solved_board.set(&empty_tile_location, TileType::Empty(0));
+    solved_board.set(&empty_tile_location, IndexedValue::<TileType>::new(TileType::Empty));
 
     solved_board.empty_tile_location=empty_tile_location;
     solved_board.ignore_player_input=true;
--- src/logic/data_structure/indexed_value.rs
+++ src/logic/data_structure/indexed_value.rs
@@ -5,3 +5,10 @@ pub struct IndexedValue<T: Copy + Eq + PartialEq + Default>{
     pub index: usize,
     pub value: T
 }
+
+impl<T: Copy + Eq + PartialEq + Default> IndexedValue<T>{
+    /// indexes with 0
+    pub fn new(value: T)-> Self{
+        Self { index: 0, value }
+    }
+}
