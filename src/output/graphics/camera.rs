3d31896c2d06b9c03c857c7f0499f605064afba4
Rabbival <87331993+Rabbival@users.noreply.github.com>
2024-05-22 18:19:18 +0300

    game settings change now only changes main camera

--- src/output/graphics/camera.rs
+++ src/output/graphics/camera.rs
@@ -11,7 +11,7 @@ impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(PostStartup, spawn_cameras)
             .add_systems(OnEnter(GameState::GameBoardGenerated),
-                 adjust_cameras_zoom_to_new_settings
+                adjust_main_camera_zoom_to_new_settings
             );
     }
 }
@@ -50,24 +50,42 @@ fn spawn_loader_slot_preview_camera(loader_slot: LoaderScreenSlot, commands: &mu
     ));
 }
 
-fn adjust_cameras_zoom_to_new_settings(
-    mut camera_query: Query<(&mut Transform, &mut OrthographicProjection), With<Camera2d>>,
+fn adjust_main_camera_zoom_to_new_settings(
+    mut camera_query: Query<
+        (&mut Transform, &mut OrthographicProjection, &LoaderSlotOwnershipTag), 
+        With<Camera2d>
+    >,
     applied_board_props_query: Query<&BoardProperties, With<AppliedBoardProperties>>,
 ) {
-    let grid_side_length = applied_board_props_query.single().size.to_grid_side_length();
-    let new_camera_zoom =
-        grid_side_length as f32 * ATLAS_CELL_SQUARE_SIZE / BOARD_SIZE_IN_PIXELS;
-
     for (
         mut camera_transform,
-        mut camera_projection
+        mut camera_projection,
+        loader_slot_tag
     )
         in &mut camera_query
     {
-        camera_transform.translation.x =
-            (grid_side_length - 1) as f32 / 2.0 * ATLAS_CELL_SQUARE_SIZE;
-        camera_transform.translation.y =
-            -1.0 * (grid_side_length - 1) as f32 / 2.0 * ATLAS_CELL_SQUARE_SIZE;
-        camera_projection.scale = new_camera_zoom;
+        if loader_slot_tag.0.is_none(){
+            set_camera_zoom_by_grid_side_length(
+                applied_board_props_query.single().size.to_grid_side_length(),
+                camera_transform.as_mut(),
+                camera_projection.as_mut(),
+            );
+            break;
+        }
     }
+}
+
+fn set_camera_zoom_by_grid_side_length(
+    grid_side_length: u8,
+    camera_transform: &mut Transform,
+    camera_projection: &mut OrthographicProjection
+){
+    let new_camera_zoom = 
+        grid_side_length as f32 * ATLAS_CELL_SQUARE_SIZE / BOARD_SIZE_IN_PIXELS;
+    
+    camera_transform.translation.x =
+        (grid_side_length - 1) as f32 / 2.0 * ATLAS_CELL_SQUARE_SIZE;
+    camera_transform.translation.y =
+        -1.0 * (grid_side_length - 1) as f32 / 2.0 * ATLAS_CELL_SQUARE_SIZE;
+    camera_projection.scale = new_camera_zoom;
 }
